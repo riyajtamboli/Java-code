4b4a8bbe0ad8855edf26b509d870580a23d9f172
riyajtamboli <tambolir97@gmail.com>
2025-11-22 09:02:11 +0530

    File uploaded

--- Day_4/gender.java
+++ Day_4/gender.java
@@ -1,7 +1,8 @@
 void main(){
 	IO.print("Enter your gender [M/F] : ");
 	String gen = IO.readln();
-	if(gen == "M"){
+	char ch = gen.charAt(0);
+	if(ch == 'M'){
 		IO.println("Your gender is : "+gen);
 		IO.print("Welcome Sir");
 	}else{
@@ -10,3 +11,4 @@ void main(){
 	}
 	  
 }
+
